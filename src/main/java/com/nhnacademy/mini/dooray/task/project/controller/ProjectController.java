ade7169487329a45844e10c912e7e29660201555
SeinJ <drasein@gmail.com>
2024-02-24 13:01:41 +0900

    auth controller 추가 수정, project controller 구현 중

--- src/main/java/com/nhnacademy/mini/dooray/task/project/controller/ProjectController.java
+++ src/main/java/com/nhnacademy/mini/dooray/task/project/controller/ProjectController.java
@@ -1,13 +1,29 @@
 package com.nhnacademy.mini.dooray.task.project.controller;
 
+import com.nhnacademy.mini.dooray.task.domain.dto.ProjectNameDto;
 import com.nhnacademy.mini.dooray.task.project.service.ProjectService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @Controller
+@RequestMapping("/projects")
 public class ProjectController {
     private final ProjectService projectService;
 
+    @PostMapping
+    public String register(@RequestBody ProjectNameDto projectNameDto){
+        ResponseEntity<String> entity = projectService.register(projectNameDto);
+        if (entity.getStatusCode().is2xxSuccessful()) {
+            return "home";
+        }
+        throw new RuntimeException("프로젝트 등록 실패 : "+entity.getStatusCode().getReasonPhrase());
+    }
 
+    @GetMapping("/{id}")
+    public String getProject(@PathVariable("id") Long id){
+        return "project.project_info";
+    }
 }
